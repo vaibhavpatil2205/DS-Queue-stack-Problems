595e81b2ec7abd2ad14c11e9952ab3830f607685
unknown <vaibhavpatil5471@gmail.com>
2021-07-04 19:12:19 +0100

    Queue Dequeue Method

---
+++ QueueDequeMethod/Queue.java
@@ -0,0 +1,69 @@
+package com.ds;
+
+public class Queue {
+
+
+		Node front;
+		Node rear;
+
+
+		class Node{
+			Node next;
+			int data;
+
+
+
+			public Node(int data) {
+				super();
+				this.next = next;
+				this.data = data;
+			}
+		}
+
+		public Node enque(int data)
+		{
+			 Node newNode = new Node(data);
+			 if (front == null && rear==null)
+			 {
+				front= rear = newNode;
+			 }
+			 else
+			 {
+				 Node temp = rear;
+				 while (temp.next != null)
+				 {
+				 temp = rear.next;
+				 }
+				 temp.next = newNode;
+			 }
+			 return newNode;
+		}
+
+		public void  dequeue()
+		 {
+			int popData = 0;
+			if (front == null && rear==null) {
+					System.out.println("Queue is empty");
+				}
+				 popData = front.data;
+				 front = front.next;
+		 }
+
+
+
+		public void print()
+		 {
+			 Node temp = front;
+			 if(temp==null)
+			 {
+				 System.out.println("Queue is empty");
+			 }
+			 while (temp != null)
+			 {
+				 System.out.print(temp.data + " -> ");
+				 temp = temp.next;
+			 }
+			 System.out.println();
+		 }
+
+	}
